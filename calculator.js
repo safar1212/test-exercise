7a5dd4dd47b2e69517a130b7969248144dbe544e
safar1212 <mm9240584@gmail.com>
2022-06-08 11:25:04 +0500

    Complete the tests for calcuator class

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
-// Class Calculator {
-//     constructor (add, subtract, devide, multiply) {
-//      this.add = add;
-//      this.subtract = subtract;
-//      this.devide = devide;
-//      this. multiply = multiply;
-//     }
-// }
+class Calculator {
+   static add = (firstNumber, secondNumber) => {
+      return firstNumber + secondNumber;
+   }
+  static subtract = (firstNumber, secondNumber) => {
+       return firstNumber - secondNumber;
+   }
+  static multiply = (firstNumber, secondNumber) => {
+       return firstNumber * secondNumber;
+   }
+  static divide = (firstNumber, secondNumber) => {
+    if (secondNumber === 0) {
+      return 'Divide by zero error';
+    }
+    return firstNumber / secondNumber;
+  }
+}
+
+module.exports = Calculator;
